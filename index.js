8aabbdab3036ddf77a6a1452ff54fc54ea7ffc87
Alpha Bah <alphabah@Alphas-MacBook-Pro.local>
2021-09-20 18:29:40 -0600

    first vrsn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 function displayColor() {
-    let colr = document.getElementById("colr");
-    let displ = document.getElementById('displ');
-    let box= document.getElementById("box")
-    displ.value = 'purple';
-    displ.style.backgroundColor = displ.value
-    box.style.backgroundColor = displ.value
+    let colrRB = document.getElementById("colrRB");
+    let displRB = document.getElementById('displRB');
+    let boxRB= document.getElementById("box")
+    displRB.value = 'Purple';
+    displRB.style.color= 'white'
+    displRB.style.backgroundColor = displRB.value
+    boxRB.style.backgroundColor = displRB.value
 }
+
+let clearRB = document.getElementById('clearRB').addEventListener('click', function () {
+    let displRB = document.getElementById('displRB');
+    displRB.style.display = 'none';
+})
